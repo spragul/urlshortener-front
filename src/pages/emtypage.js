df1a6f472d5f5e59a6e0c3a9bfb091fd11c578d8
spragul <spragul33@gmail.com>
2023-05-07 20:55:39 +0530

    commit

--- src/pages/emtypage.js
+++ src/pages/emtypage.js
@@ -3,6 +3,7 @@ export function Firstpage() {
     return (
         <div  style={{backgroundColor:"royalblue",textAlign:"center",height:"100vh"}}>
             <p style={{fontSize:'40px'}}>SignUP your Account</p>
+            <h2>WELCOME TO URLSHORTENER APP</h2>
             <Button href='/signup' variant="contained" color="success" style={{width:"200px",height:"200px",borderRadius:"30px"}}>
                 Sign UP
             </Button>
